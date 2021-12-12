741f1352f825cb7a4759444de5a6e20f271c7901
Debbl <me@aiwan.run>
2021-12-12 21:51:25 +0800

    05_0_init

---
+++ 05_webpack的模块化原理/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          "postcss-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "webpack site",
+      template: "./public/index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: [
+              '**/.DS_Store',
+              '**/index.html'
+            ]
+          }
+        }
+      ]
+    })
+  ]
+}
