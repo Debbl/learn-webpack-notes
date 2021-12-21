fe78a379f9568a0a68d1ca3acd98e0981ee4cd40
Debbl <me@aiwan.run>
2021-12-21 21:32:57 +0800

    05_4_Webapck模块化的原理

--- 05_webpack的模块化原理/build/bundle.js
+++ 05_webpack的模块化原理/build/bundle.js
@@ -1,133 +1,142 @@
-var __webpack_modules__ = {
-  './src/js/format.js': function (module) {
-    const dateFormat = (date) => {
-      return '2021-12-12';
-    };
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-    const priceFormat = (price) => {
-      return '100.00';
-    };
+/***/ "./src/js/format.js":
+/*!**************************!*\
+  !*** ./src/js/format.js ***!
+  \**************************/
+/***/ (function(module) {
 
-    module.exports = {
-      dateFormat,
-      priceFormat,
-    };
-  },
-
-  './src/js/math.js': function (
-    __unused_webpack_module,
-    __webpack_exports__,
-    __webpack_require__
-  ) {
-    'use strict';
-    __webpack_require__.r(__webpack_exports__);
-    __webpack_require__.d(__webpack_exports__, {
-      sum: function () {
-        return sum;
-      },
-      mul: function () {
-        return mul;
-      },
-    });
-    const sum = (num1, num2) => {
-      return num1 + num2;
-    };
-
-    const mul = (num1, num2) => {
-      return num1 * num2;
-    };
-  },
-};
-
-var __webpack_module_cache__ = {};
-
-function __webpack_require__(moduleId) {
-  var cachedModule = __webpack_module_cache__[moduleId];
-  if (cachedModule !== undefined) {
-    return cachedModule.exports;
-  }
-
-  var module = (__webpack_module_cache__[moduleId] = {
-    exports: {},
-  });
-
-  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-
-  return module.exports;
+const dateFormat = (date) => {
+  return '2021-12-12'
 }
 
-!(function () {
-  __webpack_require__.n = function (module) {
-    var getter =
-      // 判断是不是 esModule
-      module && module.__esModule
-        ? function () {
-            return module['default'];
-          }
-        : function () {
-            return module;
-          };
-    __webpack_require__.d(getter, { a: getter });
-    return getter;
-  };
-})();
+const priceFormat = (price) => {
+  return '100.00'
+}
 
-!(function () {
-  __webpack_require__.d = function (exports, definition) {
-    for (var key in definition) {
-      if (
-        __webpack_require__.o(definition, key) &&
-        !__webpack_require__.o(exports, key)
-      ) {
-        Object.defineProperty(exports, key, {
-          enumerable: true,
-          get: definition[key],
-        });
-      }
-    }
-  };
-})();
+module.exports = {
+  dateFormat,
+  priceFormat
+}
 
-!(function () {
-  __webpack_require__.o = function (obj, prop) {
-    return Object.prototype.hasOwnProperty.call(obj, prop);
-  };
-})();
 
-!(function () {
-  __webpack_require__.r = function (exports) {
-    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-      Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-    }
-    Object.defineProperty(exports, '__esModule', { value: true });
-  };
-})();
+/***/ }),
 
+/***/ "./src/js/math.js":
+/*!************************!*\
+  !*** ./src/js/math.js ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "sum": function() { return /* binding */ sum; },
+/* harmony export */   "mul": function() { return /* binding */ mul; }
+/* harmony export */ });
+const sum = (num1, num2) => {
+  return num1 + num2;
+}
+
+const mul = (num1, num2) => {
+  return num1 * num2;
+}
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+!function() {
+"use strict";
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_format__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/format */ "./src/js/format.js");
+/* harmony import */ var _js_format__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_format__WEBPACK_IMPORTED_MODULE_0__);
+// esModule 导出 CommonJS 导入
+const { sum, mul } = __webpack_require__(/*! ./js/math.js */ "./src/js/math.js");
 
-!(function () {
-  'use strict';
+// CommonJS 导出 esModule 导入
 
-  __webpack_require__.r(__webpack_exports__);
-  var _js_format__WEBPACK_IMPORTED_MODULE_0__ =
-    __webpack_require__('./src/js/format.js');
-  var _js_format__WEBPACK_IMPORTED_MODULE_0___default = __webpack_require__.n(
-    _js_format__WEBPACK_IMPORTED_MODULE_0__
-  );
-  // esModule 导出 CommonJS 导入
-  const math = __webpack_require__(/*! ./js/math.js */ './src/js/math.js');
 
-  // CommonJS 导出 esModule 导入
+console.log(sum(10, 20));
+console.log(mul(20, 30));
 
-  console.log(math.sum(10, 20));
-  console.log(math.mul(20, 30));
+console.log((0,_js_format__WEBPACK_IMPORTED_MODULE_0__.dateFormat)('aaa'));
+console.log((0,_js_format__WEBPACK_IMPORTED_MODULE_0__.priceFormat)('bbb'));
 
-  console.log(
-    _js_format__WEBPACK_IMPORTED_MODULE_0___default().dateFormat('aaa')
-  );
-  console.log(
-    _js_format__WEBPACK_IMPORTED_MODULE_0___default().priceFormat('bbb')
-  );
-})();
-
+}();
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
--- 05_webpack的模块化原理/src/index.js
+++ 05_webpack的模块化原理/src/index.js
@@ -1,11 +1,11 @@
 // esModule 导出 CommonJS 导入
-const math = require('./js/math.js');
+const { sum, mul } = require('./js/math.js');
 
 // CommonJS 导出 esModule 导入
-import format from './js/format';
+import { dateFormat, priceFormat } from './js/format';
 
-console.log(math.sum(10, 20));
-console.log(math.mul(20, 30));
+console.log(sum(10, 20));
+console.log(mul(20, 30));
 
-console.log(format.dateFormat("aaa"));
-console.log(format.priceFormat("bbb"));
+console.log(dateFormat('aaa'));
+console.log(priceFormat('bbb'));
